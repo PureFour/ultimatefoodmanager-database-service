678c227c94fcce08d59379c58395be4cbe9deb23
daniel <daniel.baziak@student.uj.edu.pl>
2020-12-20 16:27:49 +0100

    gitlab-ci deploy

---
+++ src/test/sample-test.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('Science', () => {
+	it('works', () => {
+		// tslint:disable-next-line:no-unused-expression
+		expect(true).to.be.true;
+	});
+});
